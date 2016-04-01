17e9c482c636ef9036b75a8e72a28c345d279eda
obliviusm <obliviusm@gmail.com>
2016-04-01 12:03:17 -0400

    add some additional space to grep palm and all arm

--- demo_arms.m
+++ demo_arms.m
@@ -2,14 +2,16 @@ load examples.mat
 imgdir = './images/';
 
 i = 3100;
+i = randi(length(examples));
 
 img = imread([imgdir,'/',examples(i).filepath]);
 cla, imagesc(img), axis image, hold on
 
 % display torso detected by berkeley poselets
 % plotbox(examples(i).torsobox,'w--')
-leftArmParts = examples(i).coords(:,lookupPart('lsho','lelb','lwri'));
-leftArmBox = makeBox(leftArmParts);
+% leftArmParts = examples(i).coords(:,lookupPart('lsho','lelb','lwri'));
+% leftArmBox = makeBox(leftArmParts);
+leftArmBox = makeArmBox(examples(i));
 plotbox(leftArmBox,'w--')
 % myplot(armParts,'go-','linewidth',3)
 
